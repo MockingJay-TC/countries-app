a2e4060d4a61daa3462b47ee9ad3633385f9d95a
MockingJayTC <victoraremu123@gmail.com>
2023-11-08 20:41:10 +0000

    add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
         nunito: ["Nunito Sans", "sans-serif"],
       },
-      colors: {
-        "dark-blue": "hsl(209, 23%, 22%)",
-        "very-dark-blue(DMB)": "hsl(207, 26%, 17%)",
-        "very-dark-blue(LMT)": "hsl(200, 15%, 8%)",
-        "dary-grey": "hsl(0, 0%, 52%)",
-        "very-light-gray": "hsl(0, 0%, 98%)",
+      textColor: {
+        skin: {
+          base: "var(--color-text-base)",
+          muted: "var(--color-text-muted)",
+        },
+      },
+      backgroundColor: {
+        skin: {
+          fill: "var(--color-fill)",
+          navbar: "var(--color-fill-navbar)",
+          card: "var(--color-card-accent)",
+        },
+      },
+      boxShadow: {
+        navbar: "0px 0px 7px 2px rgba(0, 0, 0, 0.03)",
       },
       screens: {
         mobile: "375px",
